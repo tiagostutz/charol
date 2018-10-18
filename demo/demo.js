48d97cfb0241e0ed7d8ba208d9788a628b9813c1
tiagostutz <tiagostutz@bb.com.br>
2018-10-18 20:38:08 -0300

    adding demo file

---
+++ demo/demo.js
@@ -0,0 +1,24 @@
+const charol = require('charol')
+
+let topicTree = { 
+    car: { 
+        engine: { 
+            temperature: null
+        },
+        transmission: {
+            current: null
+        },
+        status: null
+    } 
+}
+
+topicTree = charol(topicTree)
+
+console.log(topicTree.car._path)
+console.log(topicTree.car.engine._path)
+console.log(topicTree.car.engine.temperature._path)
+console.log(topicTree.car.status._path);
+
+
+
+
